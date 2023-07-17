b85b6dde82be5c7da7beb42f3e9f33cf6864c6d9
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-17 15:03:53 +0800

    space complexity O(n) example with array

---
+++ Day_1/BigO_Double.js
@@ -0,0 +1,9 @@
+const double = (array) => {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(2 * arr[1]);
+  }
+  return newArr;
+};
+
+// O(n) 1 assignment, but new array grows according to n
