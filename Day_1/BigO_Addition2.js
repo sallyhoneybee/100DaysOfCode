e251821d8569500789ca4c7df6cd33f4d8c66b20
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-17 12:13:44 +0800

    Created second addition solution with timer

---
+++ Day_1/BigO_Addition2.js
@@ -0,0 +1,8 @@
+const addUpTo = (n) => {
+  return (n * (n + 1)) / 2;
+};
+
+let t1 = performance.now();
+addUpTo(100000000);
+let t2 = performance.now();
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
