7aac4e2696907d1c170a70cb342fac2d363f8aa4
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-18 10:16:41 +0800

    example on how to use t5 questions to understand the question (no coding here)

---
+++ Day_2/Problem_Solving_Simple_Addition_Example.js
@@ -0,0 +1,17 @@
+// ==============================================================
+// WRITE A FUNCTION WHICH TAKES TWO NUMBERS AND RETURNS THEIR SUM
+// ==============================================================
+
+// 1. Can I restate the problem in my own words?
+// "Add two numbers together"
+
+// 2. What are the inputs that go into the problem? What do the inputs look like
+// are they integer numbers? or are we talking about floating points? it also depends on what language you are working in. In some languages there is an upper bound for the size of numbers so maybe addition in form of strings would work better.
+
+// 3. What are the outputs that should come from the solution to the problem? What do they look like
+
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+//  if no, do we add 0? do we return undefined or null or something? do we ask someone?
+
+// 5. How should I label the important pieces of data that are part of the problem? What should we call them? What terminology to use?
+// function called add, arguments called num1 num2, results is what we return
