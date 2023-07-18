c2294f079170402b680bb040334e097a3e2ae7c8
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-18 10:58:56 +0800

    example on step 3 of problem solving , break it down, no code

---
+++ Day_2/Problem_Solving_S3_Count_String .js
@@ -0,0 +1,19 @@
+// ===========================================================================================
+// WRITE A FUNCTION WHICH TAKES IN A STRING AND RETURNS COUNTS OF EACH CHARACTER IN THE STRING
+// ===========================================================================================
+// Step 3 - Break it down
+
+//  clarifications - not case sensitive, only alphanumeric, no symbols, no spaces
+
+const charCount = (str) => {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string, values should be the counts for those characters
+};
+const charCount = (str) => {
+  // make object to be returned at the end
+  // loop over string, for each character...
+  // if the char is a number/letter AND a key in the object, add one to count
+  // if the char is a number/letter AND not in the object, add it and set value to 1
+  //  if character is not a number/letter, dont do anything
+  // return object
+};
