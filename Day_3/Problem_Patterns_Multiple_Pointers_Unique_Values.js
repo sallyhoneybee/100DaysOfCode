35224336bac023792228b46aa3be48e12153994b
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-19 12:48:27 +0800

    an example using the multi pointer (i,j) approach to search for the number of unique values in an array.

---
+++ Day_3/Problem_Patterns_Multiple_Pointers_Unique_Values.js
@@ -0,0 +1,21 @@
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+const countUniqueValues = (arr) => {
+  // if i = j then j + i but if j > i then i + 1, max j is 9
+  let i = 0;
+  let j = 1;
+
+  while (j < arr.length) {
+    if (arr[i] === arr[j]) {
+      j++;
+    } else {
+      i++;
+      arr[i] = arr[j];
+      j++;
+    }
+  }
+
+  return i + 1;
+};
+
+console.log(countUniqueValues([-1, -1, 1, 1, 2, 3, 4, 5, 5]));
