11755aff483ba978b78fee9088c942f0a58069fa
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-21 12:53:43 +0800

    called calculate handler prop from form.js

--- Day_4/React/src/Components/Form.js
+++ Day_4/React/src/Components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
   const [inputValue, setInputValue] = useState({
     currentSavings: "",
     yearlySavings: "",
@@ -9,7 +9,7 @@ const Form = () => {
   });
   const submitHandler = (event) => {
     event.preventDefault();
-    calculateHandler(inputValue);
+    props.onCalculate(inputValue);
   };
 
   const inputChangeHandler = (event) => {
@@ -18,34 +18,6 @@ const Form = () => {
       ...prevData,
       [name]: value,
     }));
-  };
-
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
-
-    const yearlyData = []; // per-year results
-
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-
-    // do something with yearlyData ...
-    console.log(yearlyData);
   };
 
   return (
