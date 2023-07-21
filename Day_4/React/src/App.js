4425bf9b1eb846e107977cecfe34202d3f2a98a9
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-21 12:53:22 +0800

    passed calculate investment funciton as a prop to form and passed the results as props to results component

--- Day_4/React/src/App.js
+++ Day_4/React/src/App.js
@@ -1,8 +1,44 @@
+import React, { useState } from "react";
+
 import logo from "./assets/investment-calculator-logo.png";
 import Form from "./Components/Form";
 import Results from "./Components/Results";
 
 function App() {
+  const [yearlyResults, setYearlyResults] = useState();
+
+  const calculateHandler = (userInput) => {
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput["currentSavings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearlySavings"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expectedInterest"] / 100;
+    const duration = +userInput["investmentDuration"];
+    let totalInterest = 0;
+    let investedContribution = 0;
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      totalInterest += yearlyInterest;
+      currentSavings += yearlyInterest + yearlyContribution;
+      investedContribution += yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        investedContribution: investedContribution,
+        totalInterest: totalInterest,
+      });
+    }
+
+    // do something with yearlyData ...
+    setYearlyResults(yearlyData);
+  };
+
   return (
     <div>
       <header className="header">
@@ -10,12 +46,12 @@ function App() {
         <h1>Investment Calculator</h1>
       </header>
 
-      <Form />
+      <Form onCalculate={calculateHandler} />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <Results />
+      <Results results={yearlyResults} />
     </div>
   );
 }
