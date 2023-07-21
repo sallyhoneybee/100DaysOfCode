27835901e4ecff21d719a72b23c49dbefefde207
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-21 11:20:38 +0800

    created form and results component separate from app.js

---
+++ Day_4/React/src/Components/Form.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [inputValue, setInputValue] = useState({
+    currentSavings: "",
+    yearlySavings: "",
+    expectedInterest: "",
+    investmentDuration: "",
+  });
+  const submitHandler = (event) => {
+    event.preventDefault();
+    calculateHandler(inputValue);
+  };
+
+  const inputChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setInputValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const calculateHandler = (userInput) => {
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+
+    // do something with yearlyData ...
+    console.log(yearlyData);
+  };
+
+  return (
+    <form className="form" onSubmit={submitHandler}>
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            type="number"
+            id="current-savings"
+            name="currentSavings"
+            onChange={inputChangeHandler}
+            value={inputValue.currentSavings}
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            type="number"
+            id="yearly-contribution"
+            name="yearlySavings"
+            onChange={inputChangeHandler}
+            value={inputValue.yearlySavings}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            type="number"
+            id="expected-return"
+            name="expectedInterest"
+            onChange={inputChangeHandler}
+            value={inputValue.expectedInterest}
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            type="number"
+            id="duration"
+            name="investmentDuration"
+            onChange={inputChangeHandler}
+            value={inputValue.investmentDuration}
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
+
+export default Form;
