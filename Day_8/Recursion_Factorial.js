d31f37f7f69e32dcdaeaa6f9528aba9180756d11
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-07-24 12:31:26 +0800

    factorial with recursion

--- Day_8/Recursion_Factorial.js
+++ Day_8/Recursion_Factorial.js
@@ -1,9 +1,16 @@
 // Factorial without recursion
 
+// const factorial = (num) => {
+//   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return i;
+// };
+
+// Factorial with recursion
+
 const factorial = (num) => {
-  let total = 1;
-  for (let i = num; i > 1; i--) {
-    total *= i;
-  }
-  return i;
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 };
